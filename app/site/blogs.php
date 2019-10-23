172a8826a977a9b9432446bdfeef5d3b4da01d79
keldo <scilley@outlook.com>
2019-10-23 14:21:45 -0400

    minor code updates and add blog controller

---
+++ app/site/blogs.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace site;
+
+// We need the database
+use database\db;
+
+class blogs
+{
+	public function getBlogs()
+	{
+		$db = new db();
+		$query = "SELECT * FROM blogs ORDER BY blog_date DESC";
+		$result = $db->query($query);
+		return $result;
+	}
+	
+	public function last_five_blogs()
+	{
+		$db = new db();
+		$query = "SELECT * FROM blogs ORDER BY blog_date DESC LIMIT 5";
+		$result = $db->query($query);
+		return $result;
+	}
+	
+	public function popular_blogs()
+	{
+		$db = new db();
+		$query = "SELECT * FROM blogs ORDER BY loves LIMIT 5";
+		$result = $db->query($query);
+		return $result;
+	}
+	
+	public function getBlog($id)
+	{
+		$db = new db();
+		$query = "SELECT * FROM blogs WHERE id = '$id'";
+		$result = $db->query($query);
+		return $result;
+	}
+	
+	public function get_commentCount($id)
+	{
+		$db = new db();
+		$query = "SELECT count(*) as total from blog_comments WHERE blog_id = '$id'";
+		$result = $db->query($query);
+		$data = $db->fetch_assoc($result);
+		$count = $data['total'];
+		return $count;
+	}
+}
+?>
