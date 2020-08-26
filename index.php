945841b728747d0466bf632451317e51c0584983
Keldo <scilley@outlook.com>
2020-08-26 19:02:12 -0400

    fixed nav issue

--- index.php
+++ index.php
@@ -41,33 +41,6 @@ $instagram = $settings->instagram;
 $timezone = $settings->time_zone;
 // Error Checking
 $core->check_errors(true);
-$query = "SELECT * FROM mc_plugins WHERE plugin_status = 1";
-   $result = $db->query($query);
-   $finished = false;
-   
-   while($row = mysqli_fetch_assoc($result))
-   {
-      $plugin = $row['plugin_slug'];
-   }
-switch($d)
-{
-   case ''.$plugin.'':
-      $content = 'mc-content/plugins/'.$plugin.'/'.$plugin.'.php';
-      break;
-      
-   case 'error':
-	$content = 'mc-includes/core_plugins/error/error.php';
-	break;
-	
-   case 'login':
-	$content = 'mc-includes/core_plugins/account/login.php';
-	break;
-	
-   default:
-	$content = 'mc-content/themes/'.$theme.'/front_page.php';
-	break;
-
-}
 
  // Include the theme's Header
 include 'mc-content/themes/'.$theme.'/header.php';
@@ -83,7 +56,64 @@ if (isset($_REQUEST['_SESSION']))
 // Set the Desired Time Zone
 date_default_timezone_set($timezone);
 
-include $content;
+$query = "SELECT * FROM mc_plugins WHERE plugin_status = 1";
+$result = $db->query($query);
+$finished = false;
+while($row = mysqli_fetch_assoc($result))
+{
+    $plugin = $row['plugin_slug'];
+	
+    if ($d === $plugin)
+	{
+        include 'mc-content/plugins/'.$plugin.'/'.$plugin.'.php';
+        $finished = true;
+        break;
+    }
+	if ($d === 'manage')
+	{
+		include 'modules/manage/main.php';
+		$finished = true;
+		break;
+	}
+	if ($d === 'error')
+	{
+		include 'mc-includes/core_plugins/error/error.php';
+		$finished = true;
+		break;
+	}
+	if ($d === 'login')
+	{
+		include 'mc-includes/core_plugins/account/login.php';
+		$finished = true;
+		break;
+	}
+	if ($d === 'logout')
+	{
+		include 'mc-includes/core_plugins/account/logout.php';
+		$finished = true;
+		break;
+	}
+	if ($d === 'signup')
+	{
+		include 'mc-includes/core_plugins/account/signup.php';
+		$finished = true;
+		break;
+	}
+	if ($d === 'blogs')
+	{
+		include 'mc-includes/core_plugins/blogs/main.php';
+		$finished = true;
+		break;
+	}
+}
+
+if (!$finished)
+{
+    include 'mc-content/themes/'.$theme.'/front_page.php';
+}
+
+
+//include $content;
 
 
 // Include the themes Footer
