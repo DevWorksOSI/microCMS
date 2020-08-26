0990fa4f53ed2734b5ea9b2d500f636d3de3ea49
MsB101 <beckha.ma.boisvert@gmail.com>
2020-08-26 17:44:09 -0400

    add example module

---
+++ mc-content/plugins/example-plugin/example-plugin.php
@@ -0,0 +1,17 @@
+<?php
+/*
+Plugin name: Example Plugin
+Text Domain: example-plugin
+Author: Class Rebecca Boisvert <rboisvert@dwosi.us>
+Author URI: https://www.dwosi.us
+License: GNU General Public License v2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+Description: This is the example module for microCMS
+Requires at least: Since 1.5
+Requires PHP: 7.4.8
+Version: 0.1.0
+*/
+
+
+// include public template
+include('mc-content/plugins/example-plugin/public/example-plugin-public.php'));
